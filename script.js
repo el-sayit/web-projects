c605dac58fcbf0fcba0c097f481e115973375c9e
el-sayit <tmanbros@gmail.com>
2025-02-19 23:50:00 -0600

    functioning search button

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3587287e55556084244f6774a7502075&page=1'
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=3587287e55556084244f6774a7502075&query="'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=3587287e55556084244f6774a7502075&query="'
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+getMovies(API_URL)
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
     console.log(data.results)
 }
+form.addEventListener('submit',(e) => {
+    e.preventDefault()
+const searchTerm = search.value;
+if(searchTerm && searchTerm !== ''){
+    getMovies(SEARCH_URL + searchTerm)
+    search.value ='';
+}else{
+    window.location.reload()
+
+}
+} )
